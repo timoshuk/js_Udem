db25ae0a8f0f15cecad410ad7498c77961a7aee0
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-02-27 12:28:34 +0200

    finish splitUp Event

--- budget-app/src/js/main.js
+++ budget-app/src/js/main.js
@@ -1,5 +1,6 @@
 let splitUp = document.getElementById("start");
 let valueData = document.querySelectorAll(".result-table div[class$='-value']");
+let budgetValue = valueData[0];
 let expensesItem = document.querySelectorAll(".expenses-item");
 let btn = document.getElementsByTagName("button");
 let approve = btn[0];
@@ -11,3 +12,32 @@ let savings = document.querySelector("#savings");
 let sum = document.querySelector("#sum");
 let percent = document.querySelector("#percent");
 let timeData = document.querySelectorAll(".time-data [class$='-value']");
+let yearValue = timeData[0];
+let monthValue = timeData[1];
+let dayValue = timeData[2];
+
+let appData = {
+  budget: 0,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  timeData: "",
+  savings: true
+};
+
+// Events
+
+splitUp.addEventListener("click", function() {
+  let money = +prompt("Ваш бюджет на місяць", "");
+  let time = prompt("Введіть дату в форматі YYYY-MM-DD", "");
+
+  while (isNaN(money) || money == "" || money == null) {
+    money = +prompt("Ваш бюджет на місяць", "");
+  }
+  appData.budget = money;
+  appData.timeData = time;
+  budgetValue.textContent = money.toFixed();
+  yearValue.value = new Date(Date.parse(time)).getFullYear();
+  monthValue.value = new Date(Date.parse(time)).getMonth() + 1;
+  dayValue.value = new Date(Date.parse(time)).getDate();
+});
