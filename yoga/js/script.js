85a22aa052d089bba7669fffa10e6b3eadd082e8
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-03-02 18:55:35 +0200

    Finish tabs

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,17 +1,20 @@
 window.addEventListener("DOMContentLoaded", function() {
   "use strict";
   let info = document.querySelector(".info");
-  let tabs = info.querySelectorAll(".info-tabcontent");
+  let tabs = info.querySelectorAll(".info-header-tab");
   let tabsContent = document.querySelectorAll(".info-tabcontent");
 
   hideTabContent(1);
 
   info.addEventListener("click", function(e) {
     let targetEl = e.target;
-    for (let i = 0; i < tabs.length; i++) {
-      if (targetEl === tabs[i]) {
-        hideTabContent(0);
-        showTabContent(i);
+
+    if (targetEl && targetEl.classList.contains("info-header-tab")) {
+      for (let i = 0; i < tabs.length; i++) {
+        if (targetEl === tabs[i]) {
+          hideTabContent(0);
+          showTabContent(i);
+        }
       }
     }
   });
@@ -22,8 +25,10 @@ window.addEventListener("DOMContentLoaded", function() {
       tabsContent[i].classList.add("hide");
     }
   }
-  function showTabContent(num) {
-    tabsContent[num].classList.remove("hide");
-    tabsContent[num].classList.add("show");
+  function showTabContent(b) {
+    if (tabsContent[b].classList.contains("hide")) {
+      tabsContent[b].classList.remove("hide");
+      tabsContent[b].classList.add("show");
+    }
   }
 });
